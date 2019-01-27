d3c02b68e85c93b01912b0302a4eb47094d32b34
adslin <shihpei.ling@gmail.com>
2019-01-27 14:40:22 -0800

    add php files

---
+++ php/include/footer.php
@@ -0,0 +1,17 @@
+	<!-- Begin Footer -->
+	<div id="footer">
+			<a href="index.html">home</a> |
+			<a href="about.html">about</a> |
+			<a href="movie.html">my_movies</a> |
+			<a href="friends.html">friends</a>
+			<a href="blog.html">blog</a> |
+			<a href="contact.html">contact</a>
+		<hr />
+			<small>
+				 &copy; 2019, All Rights Reserved
+			</small>
+	</div><!-- End Footer -->
+</div>
+<!-- End Wrapper -->
+</body>
+</html>
---
+++ php/include/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Group 2 - Movie</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+     <!-- Begin Header -->
+         <div id="header">
+		         <h1>i_love_movies</h1>
+			 <!-- Begin Navigation -->
+    		     <div id="main-nav">
+    		        <ul>
+    		            <li><a href="index.php">home</a></li>
+    		            <li><a href="about.php">about</a></li>
+    		            <li><a href="movie.php">movies</a></li>
+    					      <li><a href="friends.php">friends</a></li>
+    		            <li><a href="blog.php">blog</a></li>
+    		            <li><a href="contact.php">contact</a></li>
+    		        </ul>
+    		      </div>
+    			  <!-- End Navigation -->
+    		 </div>
+    		 <!-- End Header -->
