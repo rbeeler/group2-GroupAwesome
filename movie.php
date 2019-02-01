7b8f75c8975d2f91749616a834f8481d2712cc16
Thomas <tcoducayen@gmail.com>
2019-01-31 19:55:00 -0800

    reorganized all of the folders so there is less clutter

--- movie.php
+++ movie.php
@@ -6,33 +6,34 @@
 </head>
 
 <body>
-  
-   
+
+
          <!-- Begin Header -->
          <div id="header">
-		 
+
 		 <h1>i_love_movies</h1>
-		 
-		 	 <!-- Begin Navigation -->    	
+
+		 	 <!-- Begin Navigation -->
 		     <div id="main-nav">
-		        <ul>			
+		        <ul>
 		            <li><a href="index.html">home</a></li>
 		            <li><a href="about.html">about</a></li>
 		            <li><a href="movie.php">movies</a></li>
 					<li><a href="friends.html">friends</a></li>
 		            <li><a href="blog.html">blog</a></li>
 		            <li><a href="contact.html">contact</a></li>
-		        </ul>	
-		      </div> 
-		     <!-- End Navigation --> 
+		        </ul>
+		      </div>
+		     <!-- End Navigation -->
 		 </div>
 		 <!-- End Header -->
-		 
+
 		  <!-- Begin Wrapper -->
    <div id="wrapper">
-		 
-		<?php 
-			
+
+		<!--This is the database area-->
+		<?php
+
 			$servername = "localhost:8889";
 			$username = "root";
 			$password = "root";
@@ -45,23 +46,26 @@
 				die("Database connection failed: " . mysqli_connect_error());
 			}
 
-			$sql = "SELECT title, rating, genre FROM MovieInfo";
+			$sql = "SELECT title, rating, genre, imageReference
+							FROM MovieInfo";
 			$result = $conn->query($sql);
 
 			if ($result->num_rows > 0) {
 				// output data of each row
 				while($row = $result->fetch_assoc()) {
-					echo "title: " . $row["title"]. " - rating: " . $row["rating"]. " - genre " . $row["genre"]. "<br>";
+					echo "title: " . $row["title"]. " - rating: " . $row["rating"]. " - genre " . $row["genre"].
+					//$image =
+					"<br>";
 				}
 			} else {
 				echo "0 results";
 			}
 			$conn->close();
-	
+
 		?>
-		 
+
    </div>
    <!-- End Wrapper -->
-   
+
 </body>
 </html>
