29ba325a929770c7eda1f62ea6f56da62d608c2b
adslin <shihpei.ling@gmail.com>
2019-02-04 14:17:04 -0800

    add sidebar.php

---
+++ php/sidebar.php
@@ -0,0 +1,15 @@
+<!-- Add Search Box for SQL -->
+	<h4>Search Over 5,000 Movies</h4>
+	<form action = "search.php" method="POST">
+	  <input type="text" name="search">
+	  <input type="submit" name="submit" value="Go">
+	</form>
+	<br>
+<!-- Add an Amazon Search Box -->
+	<form action="http://www.amazon.com/exec/obidos/external-search/" method="get" target="_blank>
+		<input type="hidden" name="mode" value="blended">
+		<input type="hidden" name="tag" value="insert associate tag">
+		<h4>Search Amazon.com:</h4>
+		<input type="text" name="keyword" size="10" value="">
+		<input type="submit" value="Go">
+	</form>
