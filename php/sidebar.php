ae279edc4316b7627b3b2a4921ffde80c8342e53
adslin <shihpei.ling@gmail.com>
2019-02-05 14:28:52 -0800

    change footer and form styles

--- php/sidebar.php
+++ php/sidebar.php
@@ -1,5 +1,5 @@
 <!-- Add Search Box for SQL -->
-	<h4>Search Over 5,000 Movies</h4>
+	<h4>Search Over 5,000 Movies:</h4>
 	<form action = "search.php" method="POST">
 	  <input type="text" name="search">
 	  <input type="submit" name="submit" value="Go">
@@ -13,3 +13,10 @@
 		<input type="text" name="keyword" size="10" value="">
 		<input type="submit" value="Go">
 	</form>
+	<br>
+<!-- Database download -->
+<h4>Free downloads:</h4>
+<p><a href="doc/movies.sql" target="_blank">movies SQL</a></p>
+<p><a href="doc/friends.sql" target="_blank">friends SQL</a></p>
+<p><a href="doc/tmdb_5000_credits.csv" target="_blank">credits excel</a></p>
+<p><a href="doc/tmdb_5000_movies.csv" target="_blank">movies excel</a></p>
